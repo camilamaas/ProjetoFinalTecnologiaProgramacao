59520bae2a92abc91e7a7f431fad130bedf4dc6b
Camila <Camila@DESKTOP-CB91C1B>
2017-08-08 21:54:02 -0300

    Add project files.

---
+++ ProjetoFinalFaculdade/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjetoFinalFaculdade.Startup))]
+namespace ProjetoFinalFaculdade
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
